15e9825f3e44144f3d165e76eab607f4f210c081
p998ugh <suminskutis@gmail.com>
2016-02-09 18:24:39 +0200

    Nothing done

--- src/com/swedbank/BowlingCalculator.java
+++ src/com/swedbank/BowlingCalculator.java
@@ -1,6 +1,50 @@
 package com.swedbank;
 
-public abstract class BowlingCalculator {
-    public abstract void roll(int score);
-    public abstract String getResult();
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class BowlingCalculator {
+
+    private int currentScore = 0;
+    private int ballsLeft = 2;
+    private int lastRoll = 0;
+    private int currentRoll = 0;
+    private int frameScore = 0;
+    private List<Integer> framesScore = new ArrayList<>();
+    private int frameCount = 0;
+
+    public void roll(int score){
+        currentRoll = score;
+        countScore(currentRoll);
+    }
+
+    public void countScore(int currentRoll){
+
+        if(lastRoll == 10){
+            currentScore += currentRoll + currentRoll;
+
+        }else {currentScore += currentRoll;
+        }
+        lastRoll = currentRoll;
+    }
+
+    public int getCurrentScore(){
+        return currentScore;
+    }
+
+    public void setNewPlayerFrameScore(){
+        for(int i = 0; i < 10; i++){
+            framesScore.add(0);
+        }
+    }
+
+    public List<Integer> getFramesScore() {
+        return framesScore;
+    }
+
+    public void countFrameScore(){
+    }
+
+
 }
--- test/com/swedbank/BowlingCalculatorTest.java
+++ test/com/swedbank/BowlingCalculatorTest.java
@@ -1,7 +1,94 @@
 package com.swedbank;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class BowlingCalculatorTest {
 
+    private List<Integer> framesScoreFake;
+    BowlingCalculator bowlingCalculator;
+
+    @Test
+    public void testRoll() {
+        BowlingCalculator bowlingCalculator = new BowlingCalculator();
+        bowlingCalculator.roll(1);
+    }
+
+
+    @Test
+    public void testFirstFrameScoreAfterTwoRolls(){
+        BowlingCalculator bowlingCalculator = new BowlingCalculator();
+        bowlingCalculator.roll(2);
+        bowlingCalculator.roll(5);
+        bowlingCalculator.getCurrentScore();
+        assertTrue(bowlingCalculator.getCurrentScore() == 7);
+    }
+
+    @Test
+    public void testStrikeAfterTwoRollsFirstRolls(){
+        BowlingCalculator bowlingCalculator = new BowlingCalculator();
+        bowlingCalculator.roll(10);
+        bowlingCalculator.roll(3);
+        bowlingCalculator.getCurrentScore();
+        assertTrue(bowlingCalculator.getCurrentScore() == 16);
+    }
+
+    @Test
+    public void isFramesScoreArrayEqualToZero(){
+        framesScoreFake = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            framesScoreFake.add(0);
+        }
+
+        bowlingCalculator = new BowlingCalculator();
+        bowlingCalculator.setNewPlayerFrameScore();
+        assertTrue(framesScoreFake.equals(bowlingCalculator.getFramesScore()));
+    }
+
+
+    /*
+    @Test
+    public void testScoreAfterTwoRollsSpare(){
+        BowlingCalculator bowlingCalculator = new BowlingCalculator();
+        bowlingCalculator.roll(7);
+        bowlingCalculator.roll(3);
+        bowlingCalculator.getCurrentScore();
+        assertTrue(bowlingCalculator.getCurrentScore() == 10);
+    }
+*/
+
+
+
+     /* @Test
+    public void testScoreAfterThreeRollsFirstTwoRollsWasSpare(){
+        BowlingCalculator bowlingCalculator = new BowlingCalculator();
+        bowlingCalculator.roll(7);
+        bowlingCalculator.roll(3);
+        bowlingCalculator.roll(5);
+        bowlingCalculator.getCurrentScore();
+        assertTrue(bowlingCalculator.getCurrentScore() == 20);
+    }
+*/
+
+    /*
+    @Test
+    public void testBallCount(){
+        BowlingCalculator bowlingCalculator = new BowlingCalculator();
+        int ballsAtStart = 2;
+        int ballsAfterOneRoll = 1;
+        int ballsAfterTwoRolls = 0;
+        bowlingCalculator.getBallsLeft();
+        bowlingCalculator.roll(1);
+        boolean afterOneRoll =
+        bowlingCalculator.getBallsLeft();
+        bowlingCalculator.roll(1);
+        bowlingCalculator.getBallsLeft();
+        assertTrue("Are there two balls at the satrt of a frame?", bowlingCalculator);
+
+    }
+    */
 }
